79a264027b68fa4d97fca1ff3f046987acaa2865
lake3205 <sennagarrelts@hotmail.com>
2025-11-06 15:44:56 +0100

    fixed backend old code

--- backend/controllers/assumptions_controller.py
+++ backend/controllers/assumptions_controller.py
@@ -1,14 +1,9 @@
 from fastapi import APIRouter, UploadFile, status, HTTPException
-from fastapi import APIRouter, UploadFile, status, HTTPException
 
 from services.assumptions_service import AssumptionsService
-from services.test_service import TestService
 from constants.clients import Clients
 from models.assumptions import AssumptionsModel
 from constants.model_version_constants import GEMINI_MODEL_VERSION, CLAUDE_MODEL_VERSION
-from services.test_service import TestService
-from constants.clients import Clients
-from models.assumptions import AssumptionsModel
 
 router = APIRouter(prefix="/assumptions")
 assumptions_service = AssumptionsService()
@@ -48,37 +43,3 @@ async def generate_test_assumptions():
     assumptions_model.set_assumptions_json(assumptions)
 
     return assumptions_model.to_dict()
-
-@router.post("/generate", status_code=status.HTTP_200_OK)
-async def generate_assumptions(image: UploadFile, ai_model: Clients):
-    if (ai_model not in Clients):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid AI model specified.")
-    
-    assumptions_model = AssumptionsModel()
-    
-    match ai_model:
-        case Clients.CLAUDE:
-            assumptions_model.model = ai_model
-            assumptions_model.version = "claude-sonnet-4-5"
-        case _:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="AI model not supported yet.")
-        
-    assumptions = await assumptions_service.get_assumptions(assumptions_model, image)
-        
-    assumptions_model.set_assumptions_json(assumptions)
-    
-    return assumptions_model.to_dict()
-
-# Endpoint for testing purposes that returns fixed assumptions
-@router.post("/test/generate", status_code=status.HTTP_200_OK)
-async def generate_test_assumptions():
-    assumptions_model = AssumptionsModel()
-    
-    assumptions = TestService.generate_assumptions()
-    
-    assumptions_model.model = "test_model"
-    assumptions_model.version = "1.0"
-    assumptions_model.set_assumptions_json(assumptions)
-    
-    return assumptions_model.to_dict()
-    
