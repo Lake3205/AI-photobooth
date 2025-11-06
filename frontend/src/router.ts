4c0d6c1ad97c223f810cd178bc118367a02d5b07
Borys Babas <borys.babas@hva.nl>
2025-11-06 21:02:20 +0100

    Added framework for Dashboard, with customisable Cards

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,6 +1,7 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import Landing from '@/pages/Landing.vue';
 import SelfieCamera from '@/pages/SelfieCamera.vue';
+import Dashboard from '@/pages/Dashboard.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
             path: '/selfie',
             name: 'selfie',
             component: SelfieCamera,
+        },
+        {
+            path: '/dashboard',
+            name: 'dashboard',
+            component: Dashboard,
         }
     ],
 })
