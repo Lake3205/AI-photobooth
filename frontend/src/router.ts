7d8ad79f0d557895114e67c376a15012e3827abe
lake3205 <sennagarrelts@hotmail.com>
2025-11-11 23:55:21 +0100

    fixed fronend reroute issue

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -27,6 +27,7 @@ const router = createRouter({
             path: '/dashboard',
             name: 'dashboard',
             component: Dashboard,
+            meta: { requiresAuth: true, requiredRole: 'admin' }
         }
     ],
 })
