d64e9a7bb07873a539cd7ecfefe2f80de191df40
Jay Nagtegaal <jay.nagtegaal@hva.nl>
2025-12-17 20:25:25 +0100

    Fix: Fixed another small comment

--- backend/services/assumptions_service.py
+++ backend/services/assumptions_service.py
@@ -19,7 +19,7 @@ class AssumptionsService:
         self.openai_client = OpenAIClient()
 
     async def get_assumptions(self, assumptions_model: AssumptionsModel, image_bytes, mime_type, image_name, detect_face = True) -> dict:
-        # if detect_face is True:
+        # if detect_face:
         #     face_detected = await self.google_client.detect_face(image_bytes=image_bytes, mime_type=mime_type)
         #     if not face_detected.face_detected:
         #         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="No face detected")
